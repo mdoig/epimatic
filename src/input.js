54091310e62c9b235381636a11a450918dba4968
Rocky Racoon <mdoig5247@gmail.com>
2022-03-26 00:16:44 -0500

    add search results component; update page title

--- src/input.js
+++ src/input.js
@@ -3,7 +3,12 @@ import React from 'react';
 export class SearchInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = {
+      searchResultsHidden: true,
+      showImages: [],
+      showResponse: '',
+      text: '',
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
@@ -13,9 +18,19 @@ export class SearchInput extends React.Component {
   }
 
   handleClick(e) {
-    fetch(`https://api.tvmaze.com/search/shows?q=${this.state.text.toLowerCase()}`)
+    const showSearchUrl = `https://api.tvmaze.com/search/shows?q=${this.state.text.toLowerCase()}`;
+
+    fetch(showSearchUrl)
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then((data) => {
+        this.setState({
+          searchResultsHidden: false,
+          showResponse: data,
+          showImages: data.map(show => show.show.image === null ? null : show.show.image.medium)
+        });
+        console.log(this.state.showResponse);
+        console.log(this.state.showImages);
+      });
   }
 
   render() {
@@ -23,6 +38,21 @@ export class SearchInput extends React.Component {
       <div>
         <input placeholder='Search show title' value={this.state.text} onChange={this.handleChange} />
         <button onClick={this.handleClick}>Submit</button>
+        <ShowResults visibility={this.state.searchResultsHidden} showImages={this.state.showImages} />
+      </div>
+    )
+  }
+};
+
+class ShowResults extends React.Component {
+  render() {
+    return (
+      <div hidden={this.props.visibility}>
+        {this.props.showImages.map(showImgSrc => (
+          showImgSrc === null ? 
+          <img key="noImage" src="https://static.tvmaze.com/images/no-img/no-img-portrait-text.png" alt="" /> :
+          <img key={showImgSrc} src={showImgSrc} alt="" />
+        ))}
       </div>
     )
   }
