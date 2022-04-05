a3d3c03740fd9ad40ed5e6eb1bbeb684649c47d0
Rocky Racoon <mdoig5247@gmail.com>
2022-04-05 15:19:02 -0500

    deployment prep + link update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <footer className="App-licenses">
         Copyright © 2022 <a href="https://github.com/mdoig" target="_blank" rel="noopener noreferrer">Marshall Doig</a>
         <br />
-        <a href="https://github.com/mdoig/eps-from-a-hat" target="_blank" rel="noopener noreferrer">This app</a> was bootstrapped with <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>. &nbsp;
+        <a href="https://github.com/mdoig/epimatic" target="_blank" rel="noopener noreferrer">This app</a> was bootstrapped with <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>. &nbsp;
         Licensed as <a href="https://mit-license.org/" target="_blank" rel="noopener noreferrer">MIT</a>. &nbsp;
         Data provided by the <a href="https://www.tvmaze.com/" target="_blank" rel="noopener noreferrer">TVmaze.com</a> <a href="https://api.tvmaze.com/" target="_blank" rel="noopener noreferrer">API</a>. &nbsp;
         Licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-SA 4.0</a>.
