523f57419f9118a36a1c1466effd85cf044fd138
Rocky Racoon <mdoig5247@gmail.com>
2022-04-05 03:06:26 -0500

    the styling... oh god, the styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { EpisodeGenerator } from './episodeGenerator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eps From a Hat
-        </p>
-        <div><EpisodeGenerator /></div>
-      <p class="App-licenses">
+      <div className="App-main">
+        <h1 className="App-name">EpiMatic</h1>
+        <h2 className="App-description">A random TV episode generator for when you don't know what to watch</h2>
+        <div id="episode-generator"><EpisodeGenerator /></div>
+      </div>
+      <footer className="App-licenses">
         Copyright © 2022 <a href="https://github.com/mdoig" target="_blank" rel="noopener noreferrer">Marshall Doig</a>
         <br />
-        <a href="https://github.com/mdoig/eps-from-a-hat" target="_blank" rel="noopener noreferrer">This app</a> was bootstrapped with <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>.
-        Licensed as <a href="https://mit-license.org/" target="_blank" rel="noopener noreferrer">MIT</a>.
-        Data provided by the <a href="https://www.tvmaze.com/" target="_blank" rel="noopener noreferrer">TVmaze.com</a> <a href="https://api.tvmaze.com/" target="_blank" rel="noopener noreferrer">API</a>.
+        <a href="https://github.com/mdoig/eps-from-a-hat" target="_blank" rel="noopener noreferrer">This app</a> was bootstrapped with <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>. &nbsp;
+        Licensed as <a href="https://mit-license.org/" target="_blank" rel="noopener noreferrer">MIT</a>. &nbsp;
+        Data provided by the <a href="https://www.tvmaze.com/" target="_blank" rel="noopener noreferrer">TVmaze.com</a> <a href="https://api.tvmaze.com/" target="_blank" rel="noopener noreferrer">API</a>. &nbsp;
         Licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-SA 4.0</a>.
-      </p>
-      </header>
+      </footer>
     </div>
   );
 }
--- src/episodeGenerator.js
+++ src/episodeGenerator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './episodeGenerator.css';
 
 export class EpisodeGenerator extends React.Component {
   constructor(props) {
@@ -68,29 +69,36 @@ export class EpisodeGenerator extends React.Component {
   render() {
     return (
       <div>
-        <input placeholder='Search show title' value={this.state.search} onChange={this.handleChange} />
+        <input id="search-field" placeholder='Search show title' value={this.state.search} onChange={this.handleChange} />
+        <br />
         <button onClick={this.searchHandleClick}>Submit</button>
         <button onClick={this.resetHandleClick}>Reset</button>
-        <div hidden={this.state.searchResultsHidden}>
+        <div id="show-results" hidden={this.state.searchResultsHidden}>
           {this.state.shows.map(show => (
             show.image === null ?
-              <img title={show.name} key={show.id} onClick={this.showHandleClick} width="210" height="295" src="https://static.tvmaze.com/images/no-img/no-img-portrait-text.png" alt={show.id} /> :
-              <img title={show.name} key={show.id} onClick={this.showHandleClick} src={show.image.medium} alt={show.id} />
+              <img id="show-img" title={show.name} key={show.id} onClick={this.showHandleClick} src="https://static.tvmaze.com/images/no-img/no-img-portrait-text.png" alt={show.id} /> :
+              <img id="show-img" title={show.name} key={show.id} onClick={this.showHandleClick} src={show.image.original} alt={show.id} />
           ))}
         </div>
         {this.state.episode === '' ?
           <div hidden /> :
-          <div hidden={this.state.episodeHidden}>
-            <p>
-              {this.state.selectedShow} &mdash; S{this.state.episode.season} E{this.state.episode.number}
+          <div id="episode" hidden={this.state.episodeHidden}>
+            <p id="episode-number">
+              <span>
+                {this.state.selectedShow} &mdash; S{this.state.episode.season} E{this.state.episode.number}
+              </span>
               <br />
-              {this.state.episode.name}
+              <span id="episode-name">
+                {this.state.episode.name}
+              </span>
               <br />
-              Aired {this.state.episode.airdate}
+              <span id="episode-airdate">
+                Aired: {this.state.episode.airdate}
+              </span>
             </p>
             {this.state.episode.image === null ?
-              <img title={this.state.episode.name} src="https://static.tvmaze.com/images/no-img/no-img-landscape-text.png" alt={this.state.episode.id} /> :
-              <img title={this.state.episode.name} src={this.state.episode.image.medium} alt={this.state.episode.id} />
+              <img id="episode-img" title={this.state.episode.name} src="https://static.tvmaze.com/images/no-img/no-img-landscape-text.png" alt={this.state.episode.id} /> :
+              <img id="episode-img" title={this.state.episode.name} src={this.state.episode.image.original} alt={this.state.episode.id} />
             }
           </div>
         }
